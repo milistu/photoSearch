760cb273ea5b3aaa5af711c50a3c6284fc544b81
milistu <studen.m98@gmail.com>
2024-02-26 22:53:45 +0100

    Add embedding calling test to healt

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,25 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
 from pydantic import BaseModel
 
+from embedding import get_embedding
+
 app = FastAPI()
+
 
 class HealtStatus(BaseModel):
     database: bool
     model: bool
     overall: bool
 
+
 @app.get("/")
 async def root():
-    return {"message" : "Hello World"}
+    return {"message": "Hello World"}
+
 
 @app.get("/health", response_model=HealtStatus)
 async def health_check() -> HealtStatus:
+    print(get_embedding())
     db_status = True
     model_status = True
     overall_status = db_status and model_status
